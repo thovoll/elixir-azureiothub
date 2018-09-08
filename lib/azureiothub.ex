2ede2f000aed54ee93e15121bc3686cf03920fac
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-07 22:19:13 -0700

    Initial code.

---
+++ lib/azureiothub.ex
@@ -0,0 +1,18 @@
+defmodule Azureiothub do
+  @moduledoc """
+  Documentation for Azureiothub.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Azureiothub.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/azureiothub_test.exs
@@ -0,0 +1,8 @@
+defmodule AzureiothubTest do
+  use ExUnit.Case
+  doctest Azureiothub
+
+  test "greets the world" do
+    assert Azureiothub.hello() == :world
+  end
+end
