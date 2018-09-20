f2b5a1bf2bc56261412f920eede74d861c2f7160
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-19 22:14:18 -0700

    Cleanup. Check result.

--- lib/azureiothub/client.ex
+++ lib/azureiothub/client.ex
@@ -4,10 +4,11 @@ defmodule AzureIoTHub.Client do
     iot_hub_host_port = config(:iot_hub_host_port)
     device_id = config(:device_id)
     device_key = config(:device_key)
+    ttl_in_sec = 86400 # 1 day
 
     IO.puts("Connecting to #{iot_hub_host_name}:#{iot_hub_host_port} using device ID '#{device_id}'...")
 
-    Tortoise.Supervisor.start_child(
+    result = Tortoise.Supervisor.start_child(
       client_id: device_id,
       handler: {Tortoise.Handler.Logger, []},
       server: {
@@ -17,9 +18,14 @@ defmodule AzureIoTHub.Client do
         port: iot_hub_host_port
       },
       user_name: device_user_name(iot_hub_host_name, device_id),
-      password: device_password(iot_hub_host_name, device_id, device_key),
+      password: device_password(iot_hub_host_name, device_id, device_key, ttl_in_sec),
       subscriptions: []
-    )
+    ) 
+
+    case result do
+      {:ok, _pid} -> IO.puts("Started.")
+      {:error, {:already_started, _pid}} -> IO.puts("Already started.")
+    end
 
     topic = events_topic(device_id)
     IO.puts("Publishing to topic #{topic}...")
@@ -31,26 +37,22 @@ defmodule AzureIoTHub.Client do
     Application.get_env(:azureiothub, key)
   end
 
-  defp device_user_name(iot_hub_host_name, device_id) do
-    "#{iot_hub_host_name}/#{device_id}/api-version=2016-11-14"
+  defp device_user_name(host_name, device_id) do
+    "#{host_name}/#{device_id}/api-version=2016-11-14"
   end
 
-  defp device_password(iot_hub_host_name, device_id, device_key) do
-    expiry = (DateTime.utc_now() |> DateTime.to_unix()) + 86400 # one day in the future
-    
-    url_encoded_resource_uri = 
-      "#{iot_hub_host_name}/devices/#{device_id}"
-      |> URI.encode_www_form 
-
+  defp device_password(host_name, device_id, device_key, ttl_in_sec) do
+    uri = "#{host_name}/devices/#{device_id}" |> URI.encode_www_form 
+    expiry = System.system_time(:second) + ttl_in_sec
+    plain_text = "#{uri}\n#{expiry}"
     decoded_device_key = device_key |> Base.decode64! 
-    plain_text = "#{url_encoded_resource_uri}\n#{expiry}"
 
-    signature_string = 
+    signature = 
       :crypto.hmac(:sha256, decoded_device_key, plain_text)
       |> Base.encode64
       |> URI.encode_www_form
 
-    "SharedAccessSignature sig=#{signature_string}&se=#{expiry}&sr=#{url_encoded_resource_uri}"
+    "SharedAccessSignature sig=#{signature}&se=#{expiry}&sr=#{uri}"
   end
 
   defp events_topic(device_id) do
