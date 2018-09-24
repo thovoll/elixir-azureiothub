6f64568de9cf3517be5610bdbf59ef772af981bb
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-24 10:04:10 -0700

    Add credo package.

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule AzureIoTHub.MixProject do
   defp deps do
     [
       {:tortoise, "~> 0.9"},
+      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
